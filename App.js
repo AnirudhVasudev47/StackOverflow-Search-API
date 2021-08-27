1a6475cd971e7711ab34f617abd03d9efa0dba9a
AnirudhVasudev47 <69135996+AnirudhVasudev47@users.noreply.github.com>
2021-08-27 09:46:46 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    view: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    textInput: {
+      width: '80%',
+      borderColor: 'black',
+      borderWidth: 1,
+      borderRadius: 10,
+      padding: 10,
+      margin: 10
+    }
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.view}>
+      <View style={{flexDirection:'row'}}>
+        <Text style={{fontSize:25, color: '#da670b'}}>Search</Text>
+        <Text style={{fontSize:25, color: '#000'}}> Stack</Text>
+        <Text style={{fontSize:25, color: '#000', fontWeight:'bold'}}>Overflow</Text>
+      </View>
+      <TextInput placeholder='test place holder'
+                 style={styles.textInput}/>
+      <Button title='ADD it' color='#da670b'/>
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
